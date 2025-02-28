b768a4ab3248d11cc6ca2c109bb07afbdae8bec0
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-02-28 11:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function convertToRoman(num) {
-  	onst obj = {
+    const obj = {
         'M': 1000,
+        'CM': 900,
         'D': 500,
+        'CD': 400,
         'C': 100,
+        'XC': 90,
         'L': 50,
+        'XL': 40,
         'X': 10,
+        'IX': 9,
         'V': 5,
+        'IV': 4,
         'I': 1
     };
 
